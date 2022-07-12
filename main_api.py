3094c45c84e442d9276f5cf02fa942e94d61a04c
VigneshRaja Muruganantham (Group) <vigneshraja.muruganantham@global.ntt>
2022-07-12 20:57:39 +0530

    changed testcases----

--- main_api.py
+++ main_api.py
@@ -14,7 +14,7 @@ import azure.functions as func
 # lm_hostname = os.environ.get('LM_Url')
 # conn_str = os.environ.get('RedisConn')
 conn_str = "rds-lm-az2-dev.redis.cache.windows.net:6380,password=MfJdAtwtjyuYg0RliL3WSVVDjoL42ugjMqZiF7NZOUU=,ssl=True,abortConnect=False"
-lm_hostname = "nttltdsandbx.logicmonitor.com"
+lm_hostname = "nttltdsandbox.logicmonitor.com"
 connStr = conn_str.split(',')
 myHostname = connStr[0].split(':')[0]
 myPassword=re.findall(r"(?<==).*",connStr[1])[0]
@@ -25,7 +25,7 @@ def generate_token(resource_path):
         # access_id = os.environ.get('LMAccessId')
         # access_key = os.environ.get('LMAccessKey')
         access_id = "5N48nz9394dHsgg688sb"
-        access_key = "XGAx48y%9uLy^tk_JfksL6E5WJ4Za]Y_9L{y{f2J"
+        access_key = "XGAx48y%9uLy^tk_JfksL6E5WJ4Za]Y_9{y{f2J"
 
         AccessId = access_id
         AccessKey = access_key
@@ -58,6 +58,7 @@ def process_lm_data():
         response = requests.request("GET", url=lm_dashboard_url, headers=headers)
         data = response.json()
         get_lm_res = data
+        print (get_lm_res)
         logging.info(data)
         status_response = response.status_code
         logging.info(status_response)
