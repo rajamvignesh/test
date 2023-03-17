c831f8f843913838682179d510b15bd083496692
Vigneshraja Muruganantham <rajamvignesh@gmail.com>
2023-03-17 11:31:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,22 @@ terraform {
   }
 }
 
-
 provider "aws" {
   profile = "default"
   region  = "ap-south-1"           # mumbai region
+}
+
+resource "aws_db_instance" "rds_instance" {
+
+    engine = "mysql"
+    engine_version = "8.0.27"
+    identifier = "rds-terraform"
+    instance_class = "db.t2.micro" 
+    storage_type = "gp2"
+    allocated_storage = 5
+    max_allocated_storage = 10 
+    name = "test"
+    username = "admin"
+    password = "admin"
+    
 }
