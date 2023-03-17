b6692a9b7684aee94da21b1b4e55a4542f84059c
Vignesh Raja  Muruganantham -IT-Business Enabler-Chennai - JBAS-Chola <vigneshraja@chola.murugappa.com>
2023-03-17 17:29:29 +0530

    Adding

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-
 provider "aws" {
   profile = "default"
   region  = var.region
@@ -39,42 +38,32 @@ resource "aws_security_group" "${var.server_name}_sg" {
   }
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.0"
-    }
-  }
-}
-
-
 resource "aws_instance" "auto_deploy_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name = "terraform-dev-server"
-  count = "1"
-  ecs_associate_public_ip_address = "false"
+      ami           = var.ami_id
+      instance_type = var.instance_type
+      key_name = "terraform-dev-server"
+      count = "1"
+      ecs_associate_public_ip_address = "false"
 
 
-  root_block_device {
-    delete_on_termination = true
-    volume_size = 50
-    volume_type = "gp2"
-  }
+      root_block_device {
+        delete_on_termination = true
+        volume_size = 50
+        volume_type = "gp2"
+      }
 
-  vpc_security_group_ids = [
-    var.security_grp
-  ]
+      vpc_security_group_ids = [
+        var.security_grp
+      ]
 
-  user_data = "${file(“userdata.sh”)}"
+      user_data = "${file("userdata.sh")}"
 
-  depends_on = [ var.security_grp ]
+      depends_on = [ var.security_grp ]
 
-  tags = {
-    Name = var.server_name
-    BU = "Project Name"
-    App-Name = "Project Name"
-    Project = "Project Name"
-  }
+      tags = {
+          Name = var.server_name
+          BU = "Project Name"
+          App-Name = "Project Name"
+          Project = "Project Name"
+      }
 }
