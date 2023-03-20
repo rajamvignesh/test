51937c25eb2a49a5e20a31431d28ff1dbb3155ef
Vigneshraja Muruganantham <rajamvignesh@gmail.com>
2023-03-20 10:53:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,24 +12,27 @@ provider "aws" {
   region  = var.region
 }
 
-resource "aws_security_group" "test_sg" {
-  name = "sec-grp"
-  description = "Allow HTTP and SSH traffic via Terraform"
+resource "aws_security_group" "auto-created-sg" {
+  name = "auto-created-sg"
+  description = "Allow SSH traffic via Terraform"
 
+  #Incoming traffic
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #Incoming traffic
   ingress {
     from_port   = 402
     to_port     = 402
-    protocol    = "ssh"
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #Outgoing traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -39,32 +42,33 @@ resource "aws_security_group" "test_sg" {
 }
 
 resource "aws_instance" "auto_deploy_server" {
-      ami           = var.ami_id
-      instance_type = var.instance_type
-      key_name = "terraform-dev-server"
-      count = 1
-      #ecs_associate_public_ip_address = "false"
-      tenancy = "default"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name = "terraform-dev-server"
+  count = 1
+  #ecs_associate_public_ip_address = "false"
+  tenancy = "default"
 
-      root_block_device {
-        delete_on_termination = true
-        volume_size = 50
-        volume_type = "gp2"
-        encrypted   = true
-      }
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 50
+    volume_type = "gp2"
+    encrypted   = true
+  }
 
-      vpc_security_group_ids = [
-        var.security_grp
-      ]
+  security_groups = ["auto-created-sg"]
+  user_data = "${file("userdata.sh")}"
 
-      user_data = "${file("userdata.sh")}"
+  depends_on = [ "auto-created-sg" ]
 
-      depends_on = [ var.security_grp ]
+  tags = {
+      Name = var.server_name
+      BU = "Project Name"
+      App-Name = "Project Name"
+      Project = "Project Name"
+  }
+}
 
-      tags = {
-          Name = var.server_name
-          BU = "Project Name"
-          App-Name = "Project Name"
-          Project = "Project Name"
-      }
+output "ip" {
+  value = "${aws_instance.auto_deploy_server.*.public_ip}"
 }
